be9b9773a0a1800e4a3bd33e72e8ae7596a65839
George Stoy <ge.georgiev95@gmail.com>
2021-06-06 18:25:51 +0300

    Added functionality on the Menu page

--- src/views/menu.js
+++ src/views/menu.js
@@ -1,78 +1,42 @@
+import { getMyMenu } from '../api/data.js';
 import { html } from '//unpkg.com/lit-html?module';
 
-const menuTeplate = () => html`
- <div class="menu">
-            <h1>Weekly Menu:</h1>
-            <div class="starter">
-                <h2>Starters:</h2>
-                <div class="card-table">
-                    <div class="food-card">
-                        <h3><i class="fas fa-check"></i>Zelena Salata </h3>
-                        <img src="/static/Images/salat.jpg" alt="" style="width:140px;height:120px;">
-                    </div>
-                    <div class="food-card">
-                        <h3><i class="fas fa-check"></i>Zelena Salata</h3>
-                        <img src="/static/Images/salat.jpg" alt="" style="width:140px;height:120px;">
-                    </div>
-                    <div class="food-card">
-                        <h3><i class="fas fa-check"></i>Zelena Salata </h3>
-                        <img src="/static/Images/salat.jpg" alt="" style="width:140px;height:120px;">
-                    </div>
-                    <div class="food-card">
-                        <h3><i class="fas fa-check"></i>Zelena Salata </h3>
-                        <img src="/static/Images/salat.jpg" alt="" style="width:140px;height:120px;">
-                    </div>
-                    <div class="food-card">
-                        <h3><i class="fas fa-check"></i>Zelena Salata </h3>
-                        <img src="/static/Images/salat.jpg" alt="" style="width:140px;height:120px;">
-                    </div>
-                    <div class="food-card">
-                        <h3><i class="fas fa-check"></i>Zelena Salata </h3>
-                        <img src="/static/Images/salat.jpg" alt="" style="width:140px;height:120px;">
-                    </div>
-                </div>
-            </div>
-            <div class="meal">
-                <h2>Meals:</h2>
-                <div class="card-table">
-                    <div class="food-card">
-                        <h3><i class="fas fa-check"></i>Musaka</h3>
-                        <img src="static/Images/musaka.jpg" alt="" style="width:140px;height:120px;">
-                    </div>
-                    <div class="card-table">
-                        <div class="food-card">
-                            <h3><i class="fas fa-check"></i>Musaka</h3>
-                            <img src="static/Images/musaka.jpg" alt="" style="width:140px;height:120px;">
-                        </div>
-                        <div class="card-table">
-                            <div class="food-card">
-                                <h3><i class="fas fa-check"></i>Musaka</h3>
-                                <img src="static/Images/musaka.jpg" alt="" style="width:140px;height:120px;">
-                            </div>
-                            <div class="food-card">
-                                <h3><i class="fas fa-check"></i>Musaka</h3>
-                                <img src="static/Images/musaka.jpg" alt="" style="width:140px;height:120px;">
-                            </div>
-                            <div class="food-card">
-                                <h3><i class="fas fa-check"></i>Musaka</h3>
-                                <img src="static/Images/musaka.jpg" alt="" style="width:140px;height:120px;">
-                            </div>
-                            <div class="food-card">
-                                <h3><i class="fas fa-check"></i>Musaka</h3>
-                                <img src="static/Images/musaka.jpg" alt="" style="width:140px;height:120px;">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+const menuTeplate = (meals, starters) => html`
+<div class="menu">
+    <h1>Weekly Menu:</h1>
+    ${meals.length === 0 || starters.length === 0 ?
+    html`<h2>You have not menu yet.</h2>
+    <a class="button" href="/create">Create</a>` :
+    myMenuTamplate(starters, meals)
+    }
+
+</div>
 `;
 
-const starterTamplate = (start) =>{
-    //TODO
-}
+const myMenuTamplate = (starter, meal) => html`
+    <div class="starter">
+        <h2>Starters:</h2>
+        ${starter.map(cardTemplate)}
+    </div>
+    <div class="meal">
+        <h2>Meals:</h2>
+        ${meal.map(cardTemplate)}
+    </div>
+`;
 
-export async function menuPage(ctx){
-    ctx.render(menuTeplate());
+
+const cardTemplate = (data) => html`
+<div class="card-table">
+    <div class="food-card">
+        <h3><i class="fas fa-check"></i>${data.name}</h3>
+        <img src="${data.imgUrl}" alt="" style="width:140px;height:120px;">
+    </div>
+</div>
+`;
+
+export async function menuPage(ctx) {
+    const menuId = sessionStorage.getItem('menuId')
+    const menu = await getMyMenu(menuId);
+    ctx.render(menuTeplate(menu.starters, menu.meals));
     ctx.userNav();
 }
