2621447d5519d3f5c1e4ab2c6eda6407b6cd09ea
George Stoy <ge.georgiev95@gmail.com>
2021-06-15 16:43:32 +0300

    Added functionality on The Shop List Page

--- src/views/groceryList.js
+++ src/views/groceryList.js
@@ -1,36 +1,39 @@
+import { getMyShopList } from '../api/data.js';
 import { html } from '//unpkg.com/lit-html?module';
 
-const groceryTemplate = (onClick) => html`
+const groceryTemplate = (onClick, products) => html`
     <div class="shop-container">
         <h1><i class="fas fa-shopping-basket"></i>Shoping List:</h1>
         <div @click=${onClick} class="shoping-card">
-            <div class="product-wrapper"><span class="product"><i class="fas fa-check"></i>Breed1</span> <span
-                    class="quantity">7 qty.</span></div>
-            <div class="product-wrapper"><span class="product"><i class="fas fa-check"></i>Breed</span> <span
-                    class="quantity">7 qty.</span></div>
-            <div class="product-wrapper"><span class="product"><i class="fas fa-check"></i>Breed</span> <span
-                    class="quantity">7</span></div>
-            <div class="product-wrapper"><span class="product"><i class="fas fa-check"></i>Breed</span> <span
-                    class="quantity">7</span></div>
-            <div class="product-wrapper"><span class="product"><i class="fas fa-check"></i>Breed</span> <span
-                    class="quantity">7</span></div>
-    
+            ${products.map(item)}
         </div>
     </div>
 `;
 
+const item = (data) => html`
+<div class="product-wrapper"><span class="product"><i class="fas fa-check"></i>${data[0]}</span> <span
+        class="quantity">${data[1]} qty.</span></div>
+`;
+
 export async function groceryListPage(ctx) {
-    ctx.render(groceryTemplate(onClick));
+    const shopListId = sessionStorage.getItem('shopListId')
+    const data = await getMyShopList(shopListId);
+    const products = Object.entries(data.products)
+    ctx.render(groceryTemplate(onClick, products));
     ctx.userNav();
 
     function onClick(event) {
         event.preventDefault();
         const parentElement = event.target.parentElement;
+        const iElement = parentElement.firstChild;
         if (parentElement.className === 'product') {
             parentElement.classList.add('done');
+            iElement.classList.replace('fa-check', 'fa-times');
+
         }
-        else if (parentElement.className === 'product done'){
+        else if (parentElement.className === 'product done') {
             parentElement.classList.remove('done');
+            iElement.classList.replace('fa-times', 'fa-check');
         }
 
     }
